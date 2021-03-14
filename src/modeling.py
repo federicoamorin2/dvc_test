39fcd07c2924210c93d71d8b23a7abac3b5766ef
Federico Amorin <federicoamorin@gmail.com>
2021-03-13 23:21:42 -0300

    Edit src/modeling.

--- src/modeling.py
+++ src/modeling.py
@@ -1,11 +1,19 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv("data/indice_de_emprego.csv")
 
 X, y = df["preco"], df["fruta"]
-x_train, x_test, y_train, y_test = train_test_split(df, random_state=305)
+x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=305)
 model = LogisticRegression()
-model.fit(x_train, y_train)
-model.predict(x_test)
+model.fit(x_train.values.reshape(-1, 1), y_train)
+preds = model.predict(x_test.values.reshape(-1, 1))
+
+print(
+    confusion_matrix(
+        y_test,
+        preds,
+    )
+)
